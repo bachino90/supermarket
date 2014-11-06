c56bd7ba082445cab499c6dd1e1d3573df7a7363
bachino90 <emilianobivachi@gmail.com>
2014-11-06 13:43:56 -0300

    Initial Commit

---
+++ supermarket/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  supermarket
+//
+//  Created by Emiliano Bivachi on 06/11/14.
+//  Copyright (c) 2014 Emiliano Bivachi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ supermarket/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  supermarket
+//
+//  Created by Emiliano Bivachi on 06/11/14.
+//  Copyright (c) 2014 Emiliano Bivachi. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
